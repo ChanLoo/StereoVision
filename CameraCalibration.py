e5aee49893bdf42611f127fee48d52392dc25b50
chanlo <woluch@foxmail.com>
2019-04-02 11:47:53 +0800

    'add'

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -1,11 +1,23 @@
 # coding: utf-8
-# author: ChanLo
-# date: 2019.3.29
+
+# Author: ChanLo
+# Date: 2019.3
+# Reference: https://github.com/LearnTechWithUs/Stereo-Vision
 
 import cv2
+import numpy as np
+
+# Termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteriaStereo = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Object points
+objPoints = np.zeros((9*7, 3), np.float32)
+objPoints[:, :2] = np.mgrid[0:9, 0:7].T.reshape(-1, 2)
+
 
 videoCapture = cv2.VideoCapture(cv2.CAP_DSHOW + 1)
-
+'''
 print('Showing camera feed. Click window or press any key to stop.')
 while(videoCapture.isOpened()):
     fps = 30 # an assumption
@@ -21,5 +33,6 @@ while(videoCapture.isOpened()):
             break
     else:
         break
-
+'''
 videoCapture.release()
+cv2.destroyAllWindows()  
