74e7bbd94c9ae03782dcf9554cb03c00918189d6
chanlo <woluch@foxmail.com>
2019-04-08 16:26:04 +0800

    update

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -19,7 +19,7 @@ objPoints = []
 imagePointsL = []
 imagePointsR = []
 
-# Calibration
+# Camera Calibration
 print("Starting calibration for the cameras...")
 # call all saved images
 for i in range(0, 50):
@@ -34,4 +34,67 @@ for i in range(0, 50):
         cv2.cornerSubPix(ChessImageR, cornersR, (11, 11), (-1, -1), criteria)
         imagePointsL.append(cornersL)
         imagePointsR.append(cornersR)
+
+# Determine the new values for different patameters
+# Left Side
+retL, mtxL, distL, rvecsL, tvecsL = cv2.calibrateCamera(objPoints, imagePointsL, ChessImageL.shape[::-1], None, None)
+hL, wL = ChessImageL.shape[:2]
+OmtxL, roiL = cv2.getOptimalNewCameraMatrix(mtxL, distL, (wL, hL), 1, (wL, hL))
+
+# Right Side
+retR, mtxR, distR, rvecsR, tvecsR = cv2.calibrateCamera(objPoints, imagePointsR, ChessImageR.shape[::-1], None, None)
+hR, wR = ChessImageR.shape[:2]
+OmtxR, roiR = cv2.getOptimalNewCameraMatrix(mtxR, distR, (wR, hR), 1, (wR, hR))
+
+print("Camera is ready to use.")
+
+# Calibrate the Camera for Stereo
+flags = 0
+flags |= cv2.CALIB_FIX_INTRINSIC
+
+retS, MLS, dLS, MRS, dRS, R, T, E, F = cv2.stereoCalibrate(objPoints,
+                                                        imagePointsL,
+                                                        imagePointsR,
+                                                        mtxL,
+                                                        distL,
+                                                        mtxR,
+                                                        distR,
+                                                        ChessImageL.shape[::-1],
+                                                        criteriaStereo,
+                                                        flags)
+
+rectifyScale = 0
+RL, RR, PL, PR, Q, roiL, roiR = cv2.stereoRectify(MLS, dLS, MRS, dRS, ChessImageL.shape[::-1], R, T, rectifyScale, (0, 0))
+LeftStereoMap = cv2.initUndistortRectifyMap(MLS, dLS, RL, PL, ChessImageL.shape[::-1], cv2.CV_16SC2)
+RightStereoMap = cv2.initUndistortRectifyMap(MRS, dRS, RR, PR, ChessImageR.shape[::-1], cv2.CV_16SC2)
+
+# Parameters for the StereoVision
+# Create StereoSGBM and prepare all parameters
+windowSize = 3
+minDisp = 2
+numDisp = 130 - minDisp
+stereo = cv2.StereoSGBM_create(minDisparity=minDisp,
+                                numDisparities=numDisp,
+                                blockSize=windowSize,
+                                P1=8*3*windowSize**2,
+                                P2=32*3*windowSize**2,
+                                disp12MaxDiff=5,
+                                uniquenessRatio=10,
+                                speckleWindowSize=100,
+                                speckleRange=32)
+
+# Used for the filtered image
+stereoR = cv2.ximgproc.createRightMatcher(stereo)
+
+# WLS FILTER Parameters
+lmbda = 80000
+sigma = 1.8
+visual_multiplier = 1.0
+
+wlsFilter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=strereo)
+wlsFilter.setLambda(lmbda)
+wlsFilter.setSigmaColor(sigma)
+
+
+
 
