610560d4fe5527ceaf63ab1ca45cf0488c07fd24
chanlo <woluch@foxmail.com>
2019-04-08 13:54:18 +0800

    update

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -12,27 +12,26 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 criteriaStereo = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Object points
-objPoints = np.zeros((9*7, 3), np.float32)
-objPoints[:, :2] = np.mgrid[0:9, 0:7].T.reshape(-1, 2)
+preObjPoints = np.zeros((9*7, 3), np.float32)
+preObjPoints[:, :2] = np.mgrid[0:9, 0:7].T.reshape(-1, 2)
 
+objPoints = []
+imagePointsL = []
+imagePointsR = []
 
-videoCapture = cv2.VideoCapture(cv2.CAP_DSHOW + 1)
-'''
-print('Showing camera feed. Click window or press any key to stop.')
-while(videoCapture.isOpened()):
-    fps = 30 # an assumption
-    # size = (1280, 480)
-    size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)), \
-        int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    ret, frame = videoCapture.read()
-    if ret == True:
-        frameL = frame[:, 640:, :]
-        frameR = frame[:, :640, :]
-        cv2.imshow('frame',frameL)
-        if cv2.waitKey(1)&0xFF == ord('q'):
-            break
-    else:
-        break
-'''
-videoCapture.release()
-cv2.destroyAllWindows()  
+# Calibration
+print("Starting calibration for the cameras...")
+# call all saved images
+for i in range(0, 50):
+    t = str(i)
+    ChessImageL = cv2.imread('./image/chessboard-L' + t + '.png', 0)
+    ChessImageR = cv2.imread('./image/chessboard-R' + t + '.png', 0)
+    retL, cornersL = cv2.findChessboardCorners(ChessImageL, (9, 7), None)
+    retR, cornersR = cv2.findChessboardCorners(ChessImageR, (9, 7), None)
+    if retL == True & retR == True:
+        objPoints.append(preObjPoints)
+        cv2.cornerSubPix(ChessImageL, cornersL, (11, 11), (-1, -1), criteria)
+        cv2.cornerSubPix(ChessImageR, cornersR, (11, 11), (-1, -1), criteria)
+        imagePointsL.append(cornersL)
+        imagePointsR.append(cornersR)
+
