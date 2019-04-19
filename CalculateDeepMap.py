4aaefcf58af685cbbcea1c1f48ae438af5a40fe5
chanlo <woluch@foxmail.com>
2019-04-19 13:57:53 +0800

    update

--- CalculateDeepMap.py
+++ CalculateDeepMap.py
@@ -1,21 +1,30 @@
 import cv2
 
-left = cv2.VideoCapture(0)
-right = cv2.VideoCapture(1)
+videoCapture = cv2.VideoCapture(cv2.CAP_DSHOW + 1)
+camera_width = 1280
+camera_height = 720
+videoCapture.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width*2)
+videoCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_height)
 
 while(True):
-    if not (left.grab() and right.grab()):
+    if not videoCapture.grab():
         print("No more frames")
         break
+    ret, frame = videoCapture.read()
+    frameL = frame[:, camera_width:, :]
+    frameR = frame[:, :camera_width, :]
+    grayL = cv2.cvtColor(frameL, cv2.COLOR_BGR2GRAY)
+    grayR = cv2.cvtColor(frameR, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('imgL', frameL)
+    cv2.imshow('imgR', frameR)
+    
+    #_, leftFrame = left.retrieve()
+    #_, rightFrame = right.retrieve()
 
-    _, leftFrame = left.retrieve()
-    _, rightFrame = right.retrieve()
-
-    cv2.imshow('left', leftFrame)
-    cv2.imshow('right', rightFrame)
+    #cv2.imshow('left', leftFrame)
+    #cv2.imshow('right', rightFrame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-left.release()
-right.release()
+videoCapture.release()
 cv2.destroyAllWindows()
