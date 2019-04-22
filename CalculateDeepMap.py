f07991dd9349b93809975208fb64c3fbff67224e
chanlo <woluch@foxmail.com>
2019-04-22 10:38:25 +0800

    update&add

--- CalculateDeepMap.py
+++ CalculateDeepMap.py
@@ -1,5 +1,17 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+imgL = cv2.imread('./image/chessboard-L1.png',0)
+imgR = cv2.imread('./image/chessboard-R1.png',0)
+
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL,imgR)
+plt.imshow(disparity,'gray')
+plt.show()
+
+
+'''
 videoCapture = cv2.VideoCapture(cv2.CAP_DSHOW + 1)
 camera_width = 1280
 camera_height = 720
@@ -28,3 +40,4 @@ while(True):
 
 videoCapture.release()
 cv2.destroyAllWindows()
+'''
